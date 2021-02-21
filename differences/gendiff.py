31b0c39d788f2d5a28d79c8e0bd7f44c72b050da
belousovsergey56 <belousovsergej56@gmail.com>
2021-02-21 21:15:12 +0300

    add 'format' in parser

--- differences/gendiff.py
+++ differences/gendiff.py
@@ -14,6 +14,7 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file', type=str, help='positional arguments')
     parser.add_argument('second_file', type=str, help='positional arguments')
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
     stdout.write(args.accumulate(args.integers))
 
