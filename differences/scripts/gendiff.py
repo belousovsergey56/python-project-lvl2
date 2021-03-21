ea35d5fb5df212acb8af6cadd391bc42f162ad0d
belousovsergey56 <belousovsergej56@gmail.com>
2021-03-21 23:28:22 +0300

    added imports, passing command line arguments to a function to output differences

--- differences/scripts/gendiff.py
+++ differences/scripts/gendiff.py
@@ -2,6 +2,8 @@
 import argparse
 from sys import stdout
 
+from differences.kernel import (read_file, gendiff)
+
 
 def main():
     """Use the function to display help.
@@ -16,7 +18,9 @@ def main():
     parser.add_argument('second_file', type=str, help='positional arguments')
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    stdout.write(args.accumulate(args.integers))
+    file_path_one = read_file.read_json_file(args.first_file)
+    file_path_two = read_file.read_json_file(args.second_file)
+    stdout.write(gendiff.generate_diff(file_path_one, file_path_two))
 
 
 if __name__ == '__main__':
