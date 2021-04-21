5648795d031e5d5ecab647d35bade756713addd3
belousovsergey56 <belousovsergej56@gmail.com>
2021-04-22 00:22:13 +0300

    add in fixtures module with values for test engine function 'result to string'

---
+++ differences/tests/fixtures/result_to_string_test.py
@@ -0,0 +1,8 @@
+"""Values for test."""
+
+first_validator = ['12', '17', '20', '35']
+first_answer = '{\n  12\n  17\n  20\n  35\n}\n'
+second_validator = ['host: hexlet.io', 'timeout: 50']
+second_answer = '{\n  host hexlet.io\n  timeout 50\n}\n'
+third_validator = ['follow: false', 'verbose: true']
+third_answer = '{\n  follow false\n  verbose true\n}\n'
